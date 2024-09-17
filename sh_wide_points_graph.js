de6791b39afcee2710f34cac1395977d80ef2072
alphaboot <98685782+alphaboot@users.noreply.github.com>
2024-09-17 18:14:44 +1000

    Add files via upload

---
+++ sh_wide_points_graph.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         [SH] Wide 'Points Over Time' Graph
+// @version      1.1
+// @description  Changes the 'Points Over Time' graph width to 100%
+// @author       alphabetsoup
+// @match        https://steamhunters.com/id/*/stats
+// @match        https://steamhunters.com/profiles/*/stats
+// @updateURL    https://raw.githubusercontent.com/alphaboot/userscripts/main/sh_curation_fetcher.user.js
+// @downloadURL  https://raw.githubusercontent.com/alphaboot/userscripts/main/sh_curation_fetcher.user.js
+// ==/UserScript==
+
+document.getElementById('pointCanvas').parentElement.style.width = '100%';
+
+// for browser console (requires changing display to something else as well):
+// document.getElementById('pointCanvas').parentElement.style.width = '100%'; document.getElementById('pointCanvas').style.display = 'inline-block';
