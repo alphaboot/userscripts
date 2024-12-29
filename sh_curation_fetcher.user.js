54c6d2803add4e188f8c9f359f6f67baa1efa5ab
alphabetsoup <98685782+alphaboot@users.noreply.github.com>
2024-12-29 16:33:03 +1100

    Update sh_curation_fetcher.user.js
    update ver no.

--- sh_curation_fetcher.user.js
+++ sh_curation_fetcher.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         [SH] AScouts Curation Fetcher
-// @version      2.61
+// @version      2.62
 // @description  Display curations on SteamHunters
 // @author       alphabetsoup
 // @match        https://steamhunters.com/apps/*
