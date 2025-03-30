28b97a93a5a7447a4e0492f75369e96408f509dd
alphabetsoup <98685782+alphaboot@users.noreply.github.com>
2025-03-30 14:33:27 +1100

    Update sh_wide_points_graph.user.js

--- sh_wide_points_graph.user.js
+++ sh_wide_points_graph.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         [SH] Wide 'Points Over Time' Graph
-// @version      1.2
+// @version      1.3
 // @description  Changes the 'Points Over Time' graph width to 100%
 // @author       alphabetsoup
 // @match        https://steamhunters.com/id/*/stats
@@ -10,19 +10,19 @@
 // ==/UserScript==
 
 // for browser console (requires changing display to something else as well):
-// document.getElementById('pointCanvas').parentElement.style.width = '100%'; document.getElementById('pointCanvas').style.display = 'inline-block';
+// document.getElementById('pointsChartCanvas').parentElement.style.width = '100%'; document.getElementById('pointsChartCanvas').style.display = 'inline-block';
 
 (function() {
     'use strict';
 
     // Find the canvas element by its ID
-    var canvasElement = document.getElementById('pointCanvas');
+    var canvasElement = document.getElementById('pointsChartCanvas');
 
     if (canvasElement) {
         // Get the parent element of the canvas
         var parentDiv = canvasElement.parentElement;
 
-        if (parentDiv && parentDiv.classList.contains('col-sm-6') && parentDiv.classList.contains('col-lg-4')) {
+        if (parentDiv && parentDiv.classList.contains('col-sm-6')) {
             // Change the width of the parent div to 100%
             parentDiv.style.width = '100%';
         }
